f3c787d9ec43df5ecfabffd2d37258f1698c6b44
Jossaya Camille <143653842+jcamille2023@users.noreply.github.com>
2023-12-29 15:43:20 -0500

    Update channel.js

--- channel.js
+++ channel.js
@@ -147,6 +147,10 @@ function requestPermission() {
 	if(currentToken) {
 		console.log(currentToken);
 		set(ref(database, "/push/tokens/" + uid), {token: currentToken, channel: channel_id});
+		if ('serviceWorker' in navigator) {
+ 		 navigator.serviceWorker.register('../firebase-messaging-sw.js').then((registration) => {console.log('Service Worker registered with scope:', registration.scope);}).catch((error) => 
+			 {console.error('Service Worker registration failed:', error);});
+		}
 			}  
 	else {
 		console.log("no token");    
