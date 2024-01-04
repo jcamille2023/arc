88381c16398055605176092b814eac0012fbde59
Jossaya Camille <143653842+jcamille2023@users.noreply.github.com>
2024-01-03 19:17:35 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.0/firebase-app.js";
+import { getAuth, onAuthStateChanged, signOut} from "https://www.gstatic.com/firebasejs/10.5.0/firebase-auth.js";
+
+ const firebaseConfig = {
+  apiKey: "AIzaSyC5oq9fyPeoo8jVU-N07gYhjt2kFEBGqA8",
+  authDomain: "arc-by-insight.firebaseapp.com",
+  projectId: "arc-by-insight",
+  storageBucket: "arc-by-insight.appspot.com",
+  messagingSenderId: "1073428960179",
+  appId: "1:1073428960179:web:c61897786f1d2ba05131c6",
+  measurementId: "G-47T814R2SK"
+};
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const database = getDatabase(app);
+  const auth = getAuth(app);
+  const dbRef = ref(getDatabase());
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    window.location.href = "dashboard.html";
+  }
+    // ...
+  } else {
+    window.location.href = "index.html";
+    // ...
+  }
+});
