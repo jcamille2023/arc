e42a0cb3cb895ec6ccc01c25f17dc725205d1ed9
Jossaya Camille <jcamille2023@fau.edu>
2024-08-11 17:39:00 -0400

    created my own configuration struct, to-do: create database structure

--- backend/src/main.go
+++ backend/src/main.go
@@ -7,6 +7,8 @@ import (
 
 	firebase "firebase.google.com/go/v4"
 	"firebase.google.com/go/v4/auth"
+	"firebase.google.com/go/v4/db"
+	"firebase.google.com/go/v4/messaging"
 	socketio "github.com/googollee/go-socket.io"
 )
 
@@ -14,18 +16,39 @@ var app *firebase.App
 var client *auth.Client
 var err error
 
+type firebaseConfig struct {
+	auth     *auth.Client
+	database *db.Client
+	msg      *messaging.Client
+}
+
 func main() {
+	// firebase configuration
 	conf := &firebase.Config{
 		DatabaseURL: "https://arc-by-insight-default-rtdb.firebaseio.com/",
 	}
+	// app intialization
 	app, err = firebase.NewApp(context.Background(), conf)
 	if err != nil {
 		log.Fatalf("Firebase failed to initalize.")
 	}
+
+	//
 	client, err = app.Auth(context.Background())
 	if err != nil {
 		log.Fatalf("Firebase Authentication failed to initalize.")
 	}
+
+	db, err := app.Database(context.Background())
+	if err != nil {
+		log.Fatalf("Firebase Realtime Database failed to initalize")
+	}
+	msg, err := app.Messaging(context.Background())
+	if err != nil {
+		log.Fatalf("Firebase Messaging failed to initalize.")
+	}
+
+	arcConfig := firebaseConfig{client, db, msg}
 
 	server := socketio.NewServer(nil)
 	server.OnConnect("/", func(s socketio.Conn) error {
