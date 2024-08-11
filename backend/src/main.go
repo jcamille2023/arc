9026cae87a477f1db74e2f80d46757fe1c4eca0f
Jossaya Camille <jcamille2023@fau.edu>
2024-08-11 17:01:23 -0400

    go backend is going fairly well.

--- backend/src/main.go
+++ backend/src/main.go
@@ -6,14 +6,19 @@ import (
 	"log"
 
 	firebase "firebase.google.com/go/v4"
+	"firebase.google.com/go/v4/auth"
+	socketio "github.com/googollee/go-socket.io"
 )
 
 var app *firebase.App
-var client *firebase.Auth
+var client *auth.Client
 var err error
 
 func main() {
-	app, err = firebase.NewApp(context.Background(), nil)
+	conf := &firebase.Config{
+		DatabaseURL: "https://arc-by-insight-default-rtdb.firebaseio.com/",
+	}
+	app, err = firebase.NewApp(context.Background(), conf)
 	if err != nil {
 		log.Fatalf("Firebase failed to initalize.")
 	}
@@ -22,13 +27,20 @@ func main() {
 		log.Fatalf("Firebase Authentication failed to initalize.")
 	}
 
+	server := socketio.NewServer(nil)
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		fmt.Println("New device connected to server, waiting for requests...")
+		return nil
+	})
+
 }
 
 func getUIDfromToken(t string) string {
-	token, err := client.verifyIdToken(app, t)
+	token, err := client.VerifyIDToken(context.Background(), t)
 	if err == nil {
 		fmt.Println("Token retrieval failed.")
 		return "403"
 	}
-	return token.uid
+	return token.UID
 }
