87cbcd5154f7be4050017cd2feb3c41dec117073
Jossaya Camille <jcamille2023@fau.edu>
2024-08-12 02:02:13 -0400

    ok im going a little crazy i will come back to this in the morning and rewrite some and add type conversion so i can pass the id into the reference path...

--- backend/src/main.go
+++ backend/src/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 
+	"time"
+
 	firebase "firebase.google.com/go/v4"
 	"firebase.google.com/go/v4/auth"
 	"firebase.google.com/go/v4/db"
@@ -20,6 +22,52 @@ type firebaseConfig struct {
 	auth     *auth.Client
 	database *db.Client
 	msg      *messaging.Client
+}
+
+type User struct {
+	firstName string
+	lastName  string
+	email     string
+	uid       string
+	photoURL  string
+	arcs      []string
+	circles   []string
+}
+
+type PrivateUser struct {
+	firstName string
+	lastName  string
+	uid       string
+	photoURL  string
+}
+
+func (s User) toPrivateUser() PrivateUser {
+	return PrivateUser{
+		firstName: s.firstName,
+		lastName:  s.lastName,
+		uid:       s.uid,
+		photoURL:  s.photoURL,
+	}
+}
+
+type Circle struct {
+	id      int32
+	name    string
+	members []PrivateUser
+	admin   []PrivateUser
+}
+type Arc struct {
+	id      int32
+	name    string
+	members [2]string
+}
+
+type Message struct {
+	id      int32
+	content string
+	author  string
+	date    time.Time
+	arc     bool
 }
 
 func main() {
@@ -56,11 +104,50 @@ func main() {
 		fmt.Println("New device connected to server, waiting for requests...")
 		return nil
 	})
-	server.OnEvent("/", "join", func(s socketio.Conn, id string, token string) {
+	server.OnEvent("/", "join", func(s socketio.Conn, id string, token string, circle bool) {
 		uid := getUIDfromToken(token)
 		if uid != "403" {
-
+			var source string
+			if circle {
+				source = "circles"
+			} else {
+				source = "arcs"
+			}
+			ref := arcConfig.database.NewRef(source + "/" + id)
+			var cir Circle
+			if err := ref.Get(context.Background(), &cir); err != nil {
+				s.Emit("error", "This Arc/Circle may not exist. Try again later.")
+			} else {
+				for _, user := range cir.members {
+					if uid == user.uid {
+						s.Join(id)
+						s.Emit(cir.name + ": Success")
+						break
+					}
+				}
+			}
+		} else {
+			s.Emit("Token verification failed.")
 		}
+	})
+	server.OnEvent("/", "send_message", func(s socketio.Conn, content string, token string, id string, circle bool) {
+		uid := getUIDfromToken(token)
+		if uid == "403" {
+			s.Emit("Token verification failed")
+			return
+		}
+		if circle {
+			var cir Circle
+			if err := ref.Get(context.Background(), &cir); err != nil {
+				s.Emit("error", "This Circle may not exist. Try again later.")
+			}
+		} else {
+			var arc Arc
+			if err := ref.Get(context.Background(), &arc); err != nil {
+				s.Emit("error", "This Circle may not exist. Try again later.")
+			}
+		}
+
 	})
 
 }
@@ -72,4 +159,10 @@ func getUIDfromToken(t string) string {
 		return "403"
 	}
 	return token.UID
+}
+
+func auth_circle(c Circle, token string, admin bool) {
+	if admin {
+		ref := arcConfig.database.NewRef("circles/" + c.id)
+	}
 }
